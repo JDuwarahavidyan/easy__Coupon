59e408bc0006eb2659ac7536589716f9d92f799c
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2024-07-13 20:44:28 +0530

    Minor changes in report page

--- lib/pages/student/student_report.dart
+++ lib/pages/student/student_report.dart
@@ -1,3 +1,5 @@
+import 'package:easy_coupon/bloc/user/user_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:easy_coupon/bloc/report/report_bloc.dart';
@@ -27,7 +29,7 @@ class _StudentReportPageState extends State<StudentReportPage> {
         return Theme(
           data: Theme.of(context).copyWith(
             colorScheme: const ColorScheme.light(
-              primary:  Color(0xFFFF8A00), // header background color
+              primary: Color(0xFFFF8A00), // header background color
               onPrimary: Colors.white, // header text color
               onSurface: Colors.black, // body text color
             ),
@@ -72,115 +74,129 @@ class _StudentReportPageState extends State<StudentReportPage> {
           ),
         ],
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          // Define a base width for iPhone 12 Pro
-          // Calculate the scale factor
-
-          return Container(
-            padding: const EdgeInsets.all(20),
-            color: const Color(0xFFF9E6BD),
-            child: Center(
-              child: Container(
-                width: constraints.maxWidth * 0.9,
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFCD170),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 20), // Spacer to push content down
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: BlocBuilder<UserBloc, UserState>(
+        builder: (context, state) {
+          if (state is UserLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is UserLoaded) {
+            final user = state.users.firstWhere(
+              (user) => user.id == FirebaseAuth.instance.currentUser?.uid,
+            );
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                return Container(
+                  padding: const EdgeInsets.all(20),
+                  color: const Color(0xFFF9E6BD),
+                  child: Center(
+                    child: Container(
+                      width: constraints.maxWidth * 0.9,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFCD170),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () =>
-                                  _selectDate(context, _startDateController),
-                              child: AbsorbPointer(
-                                child: TextField(
-                                  controller: _startDateController,
-                                  decoration: const InputDecoration(
-                                    labelText: "Start Date",
-                                    suffixIcon: Icon(Icons.calendar_today),
+                          const SizedBox(
+                              height: 20), // Spacer to push content down
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () => _selectDate(
+                                        context, _startDateController),
+                                    child: AbsorbPointer(
+                                      child: TextField(
+                                        controller: _startDateController,
+                                        decoration: const InputDecoration(
+                                          labelText: "Start Date",
+                                          suffixIcon:
+                                              Icon(Icons.calendar_today),
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () =>
-                                  _selectDate(context, _endDateController),
-                              child: AbsorbPointer(
-                                child: TextField(
-                                  controller: _endDateController,
-                                  decoration: const InputDecoration(
-                                    labelText: "End Date",
-                                    suffixIcon: Icon(Icons.calendar_today),
+                                const SizedBox(width: 20),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () => _selectDate(
+                                        context, _endDateController),
+                                    child: AbsorbPointer(
+                                      child: TextField(
+                                        controller: _endDateController,
+                                        decoration: const InputDecoration(
+                                          labelText: "End Date",
+                                          suffixIcon:
+                                              Icon(Icons.calendar_today),
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                                const SizedBox(width: 20),
+                                ElevatedButton(
+                                  onPressed: _fetchData,
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor:
+                                        const Color(0xFFFF8A00), // text color
+                                  ),
+                                  child: const Text('Fetch Data'),
+                                ),
+                              ],
                             ),
                           ),
-                          const SizedBox(width: 20),
-                          ElevatedButton(
-                            onPressed: _fetchData,
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.white,
-                              backgroundColor:
-                                  const Color(0xFFFF8A00), // text color
+                          const SizedBox(
+                              height:
+                                  20), // Spacer between date inputs and table
+                          Expanded(
+                            child: BlocBuilder<ReportBloc, ReportState>(
+                              builder: (context, state) {
+                                if (state is ReportLoaded) {
+                                  return SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: DataTable(
+                                      border: TableBorder
+                                          .all(), // Add borders to the table
+                                      columns: const [
+                                        DataColumn(label: Text('Number')),
+                                        DataColumn(label: Text('Time')),
+                                        DataColumn(label: Text('Date')),
+                                      ],
+                                      rows: state.reportData.map((data) {
+                                        return DataRow(cells: [
+                                          DataCell(Text(data.Number)),
+                                          DataCell(Text(data.Time)),
+                                          DataCell(Text(data.Date)),
+                                        ]);
+                                      }).toList(),
+                                    ),
+                                  );
+                                } else if (state is ReportLoading) {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                } else if (state is ReportError) {
+                                  return Center(
+                                      child: Text('Error: ${state.error}'));
+                                } else {
+                                  return const Center(child: Text('No data'));
+                                }
+                              },
                             ),
-                            child: const Text('Fetch Data'),
                           ),
                         ],
                       ),
                     ),
-                    const SizedBox(
-                        height: 20), // Spacer between date inputs and table
-                    Expanded(
-                      child: BlocBuilder<ReportBloc, ReportState>(
-                        builder: (context, state) {
-                          if (state is ReportLoaded) {
-                            return SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: DataTable(
-                                border: TableBorder
-                                    .all(), // Add borders to the table
-                                columns: const [
-                                  DataColumn(label: Text('Number')),
-                                  DataColumn(label: Text('Time')),
-                                  DataColumn(label: Text('Date')),
-                                ],
-                                rows: state.reportData.map((data) {
-                                  return DataRow(cells: [
-                                    DataCell(Text(data.Number)),
-                                    DataCell(Text(data.Time)),
-                                    DataCell(Text(data.Date)),
-                                  ]);
-                                }).toList(),
-                              ),
-                            );
-                          } else if (state is ReportLoading) {
-                            return const Center(
-                                child: CircularProgressIndicator());
-                          } else if (state is ReportError) {
-                            return Center(child: Text('Error: ${state.error}'));
-                          } else {
-                            return const Center(child: Text('No data'));
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+                  ),
+                );
+              },
+            );
+          }
+           return const Center(child: Text('Failed to load user data'));
         },
       ),
     );
